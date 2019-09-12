e1a103cf8ee58ef0f2ad7e53778f4d199d428d5c
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-12 14:25:11 +0200

    Added @Produces annotation so that it will return the student data in JSON Format

--- SampleRESTAPI/src/main/java/com/test/first/StudentResources.java
+++ SampleRESTAPI/src/main/java/com/test/first/StudentResources.java
@@ -1,8 +1,8 @@
 package com.test.first;
 
+import java.awt.PageAttributes.MediaType;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -15,20 +15,23 @@ public class StudentResources {
 
 	StudentRepository studentrepo = new StudentRepository();
 	
+	
+	//This method returns the list of students in JSON Format
 	@GET
 	@Path("getStudentList")
 	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 	public List<Student> getStudentInformation() {
 		// Fetching data like from the database
-		System.out.println("getStudentInformation========================");
 		return studentrepo.getStudentList();
 	}
 	
+	
+	// This method takes student bean in JSON Format and returns the student which matches with the given id
 	@POST
 	@Path("getStudentWithId")
+	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 	public Student getId(Student student) {
 		//This method returns a student that matches with the particular id
-		System.out.println("getId--------------------------------------------------");
 		return studentrepo.getStudent(student.getId());
 	}
 }
