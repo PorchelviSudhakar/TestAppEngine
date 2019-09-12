a373e931e239a2a5553118102f73402a4936363e
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-12 14:33:25 +0200

    Formatted the classes

--- SampleRESTAPI/src/main/java/com/test/first/StudentResources.java
+++ SampleRESTAPI/src/main/java/com/test/first/StudentResources.java
@@ -8,15 +8,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-
 @Path("student")
 public class StudentResources {
-	
 
 	StudentRepository studentrepo = new StudentRepository();
-	
-	
-	//This method returns the list of students in JSON Format
+
+	// This method returns the list of students in JSON Format
 	@GET
 	@Path("getStudentList")
 	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
@@ -24,14 +21,14 @@ public class StudentResources {
 		// Fetching data like from the database
 		return studentrepo.getStudentList();
 	}
-	
-	
-	// This method takes student bean in JSON Format and returns the student which matches with the given id
+
+	// This method takes student bean in JSON Format and returns the student which
+	// matches with the given id
 	@POST
 	@Path("getStudentWithId")
 	@Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 	public Student getId(Student student) {
-		//This method returns a student that matches with the particular id
+		// This method returns a student that matches with the particular id
 		return studentrepo.getStudent(student.getId());
 	}
 }
