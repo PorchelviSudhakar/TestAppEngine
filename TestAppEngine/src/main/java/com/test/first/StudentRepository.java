611b97ac1378db6ae995314fb52651892066582c
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-13 12:59:51 +0200

    This class provides list of students and particular student based on the given id

---
+++ TestAppEngine/src/main/java/com/test/first/StudentRepository.java
@@ -0,0 +1,37 @@
+package com.test.first;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class StudentRepository {
+	List<Student> studentList;
+
+	StudentRepository() {
+		Student student = new Student();
+		student.setName("student1");
+		student.setMark(80);
+		student.setId(1);
+
+		Student student1 = new Student();
+		student1.setName("student2");
+		student1.setMark(90);
+		student1.setId(2);
+		studentList = Arrays.asList(student, student1);
+	}
+
+	public List<Student> getStudentList() {
+		return studentList;
+	}
+
+	public Student getStudent(int id) {
+		
+		for (Student student : studentList) {
+			if(student.getId()==id) {
+				return student;
+			}
+		}
+		return new Student();
+	}
+}
