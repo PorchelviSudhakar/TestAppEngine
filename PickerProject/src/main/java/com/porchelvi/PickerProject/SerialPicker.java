4660400c97abd4144b795ebe9d9b39092ac23de2
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-16 21:42:03 +0200

    Created SerialPicker which returns the next element in the list when it reaches the end it will print the first element

--- PickerProject/src/main/java/com/porchelvi/PickerProject/SerialPicker.java
+++ PickerProject/src/main/java/com/porchelvi/PickerProject/SerialPicker.java
@@ -2,32 +2,34 @@ package com.porchelvi.PickerProject;
 
 import java.util.List;
 
-import javax.swing.plaf.metal.MetalIconFactory.FolderIcon16;
-
 import projects.Projects;
 
 public class SerialPicker {
 	private List<Projects> serialPickerlist;
 	private static int count = 0;
-	private static int temp = 0;
-	private static int countForExit = 0;
 
+	/*
+	 * SerialPicker Constructor Input:List<Projects>
+	 */
 	SerialPicker(List<Projects> SerialPickerlist) {
 		this.serialPickerlist = SerialPickerlist;
 		getSerialObject(serialPickerlist);
 	}
 
+	/*
+	 * getSerialObject method takes given list as input and prints the next element
+	 * in the List
+	 */
 	public static void getSerialObject(List<Projects> serialPickerlist) {
-		for (int temp = 0; temp <= serialPickerlist.size()-1; temp++) {
-			if (count == serialPickerlist.size()-1) {
-				countForExit++;
+		for (int temp = 0; temp <= serialPickerlist.size() - 1; temp++) {
+			if (count == serialPickerlist.size() - 1) {
 				temp = 0;
 				System.out.println("Retrieved Project Using SerialPicker is : " + serialPickerlist.get(temp).getName());
 				break;
-				//Recursive call leads to stack overflow
-//				getSerialObject(serialPickerlist, desiredProject);
+				// Recursive call leads to stack overflow
+				// getSerialObject(serialPickerlist, desiredProject);
 			}
-			System.out.println("Retrieved Project Using SerialPicker is : " + serialPickerlist.get(temp+1).getName());
+			System.out.println("Retrieved Project Using SerialPicker is : " + serialPickerlist.get(temp + 1).getName());
 			count++;
 		}
 
