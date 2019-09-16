fca3ddb626599a32b3a87665cdb5835d80dba34f
PorchelviSudhakar <porchelvigunasekaran@gmail.com>
2019-09-16 21:56:47 +0200

    Configurations were done in ApplicationConfig class.

--- TestAppEngine/src/main/java/com/test/first/ApplicationConfig.java
+++ TestAppEngine/src/main/java/com/test/first/ApplicationConfig.java
@@ -3,16 +3,16 @@ package com.test.first;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
-// This Class is used to create singleton Object
+/* This Class is used to create singleton Object and configurations can be done in this class like component scan in spring */
 public class ApplicationConfig extends ResourceConfig {
-    public ApplicationConfig() {
-    	System.out.println("Calling Application Config");
-        register(StudentResources.class);
-        register(new AbstractBinder() {
-            @Override
-            protected void configure() {
-            	bind(new StudentRepository()).to(StudentRepository.class);
-            }
-        });
-    }
+	public ApplicationConfig() {
+		System.out.println("Calling Application Config");
+		register(StudentResources.class);
+		register(new AbstractBinder() {
+			@Override
+			protected void configure() {
+				bind(new StudentRepository()).to(StudentRepository.class);
+			}
+		});
+	}
 }
